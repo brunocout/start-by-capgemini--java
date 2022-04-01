12fa4b14aa26f8154830efe4f1773453a3d1869c
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-04-01 04:26:29 -0300

    finishing class 02

--- aula02/App.java
+++ aula02/App.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner leitor = new Scanner(System.in);
+
+        int idade = leitor.nextInt();
+        float contacaoDolar = leitor.nextFloat();
+        double cotacaoEuro = leitor.nextDouble();
+        String nome = leitor.nextLine();
+        String codigoRua = leitor.next();
+
+        System.out.println("Texto que será exibido no console");
+        System.out.print("Texto que será exibido no console");
+        
     }
 }
