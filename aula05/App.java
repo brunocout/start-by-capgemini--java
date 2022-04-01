94e9a7ea9787a76714559b98dff2b69942eebef6
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-04-01 04:52:31 -0300

    finishing class 05

---
+++ aula05/App.java
@@ -0,0 +1,20 @@
+package aula05;
+
+public class App {
+    public static void main(String[] args) {
+        int codigoProduto = 2;
+
+        switch(codigoProduto) {
+            
+            case 1:
+
+                break;
+
+            case 2:
+
+                break;
+            
+            default:
+        }
+    }
+}
