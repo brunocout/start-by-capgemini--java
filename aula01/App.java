dde786b2b8489a71d6087d16df23bba483a71696
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-04-01 04:19:39 -0300

    first commit

---
+++ aula01/App.java
@@ -0,0 +1,9 @@
+//My first project with Java
+
+package aula01;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World");
+    }
+}
---
+++ aula02/App.java
@@ -0,0 +1,9 @@
+package aula02;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+    }
+}
